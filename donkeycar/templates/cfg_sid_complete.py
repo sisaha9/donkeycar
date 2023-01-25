088b3fee9d0546aa3a00d58ab68b066480db5744
Siddharth Saha <sisahawork@gmail.com>
2023-01-24 19:02:52 -0800

    Adding image info

--- donkeycar/templates/cfg_sid_complete.py
+++ donkeycar/templates/cfg_sid_complete.py
@@ -181,10 +181,37 @@ RECORD_ALERT_COLOR_ARR = [ (0, (1, 1, 1)),
 DONKEY_GYM = True
 DONKEY_SIM_PATH = "remote" #"/home/tkramer/projects/sdsandbox/sdsim/build/DonkeySimLinux/donkey_sim.x86_64" when racing on virtual-race-league use "remote", or user "remote" when you want to start the sim manually first.
 DONKEY_GYM_ENV_NAME = "donkey-avc-sparkfun-v0" # ("donkey-generated-track-v0"|"donkey-generated-roads-v0"|"donkey-warehouse-v0"|"donkey-avc-sparkfun-v0")
-GYM_CONF = { "body_style" : "donkey", "body_rgb" : (128, 128, 128), "car_name" : "car", "font_size" : 100} # body style(donkey|bare|car01) body rgb 0-255
-GYM_CONF["racer_name"] = "Your Name"
-GYM_CONF["country"] = "Place"
-GYM_CONF["bio"] = "I race robots."
+GYM_CONF = { "body_style" : "car01", "body_rgb" : (255, 205, 0), "car_name" : "UCSD-DSC-178-YourName", "font_size" : 30} # body style(donkey|bare|car01) body rgb 0-255
+GYM_CONF["racer_name"] = "UCSD-DSC-178-YourName"
+GYM_CONF["country"] = "USA"
+GYM_CONF["bio"] = "Representing HDSI"
+GYM_CONF["cam_config"] = {
+    "img_w": 160, # image width
+    "img_h": 120, # image height
+    "img_d": 3, # depth in dimensions
+    "img_enc": "JPG", # encoding format to send images in
+    "fov": 70.0, # The field of view
+    "fish_eye_x": 0.0, # controls distortion
+    "fish_eye_y": 0.0, # controls distortion
+    "offset_x": 0.0, #offset_x moves camera left/right
+    "offset_y": 0.0, #offset_y moves camera up/down
+    "offset_z": 0.0, #offset_z moves camera forward/back
+    "rot_x": 0.0, #rot_x will rotate the camera laterally
+    "rot_y": 0.0, #rot_y will rotate the camera up / down
+    "rot_z": 0.0, #rot_z will rotate the camera around forward vector
+}
+GYM_CONF["lidar_config"] = {
+    "deg_per_sweep_inc": 2.0, # Angular resolution
+    "deg_ang_down": 10,
+    "deg_ang_delta": -1.0,
+    "num_sweeps_levels": 1, # Number of sweeps
+    "max_range": 25.0,
+    "noise": 0.0,
+    "offset_x": 0.0,
+    "offset_y": 0.0,
+    "offset_z": 0.0,
+    "rot_x": 0.0,
+}
 
 SIM_HOST = "127.0.0.1"              # when racing on virtual-race-league use host "trainmydonkey.com"
 SIM_ARTIFICIAL_LATENCY = 0          # this is the millisecond latency in controls. Can use useful in emulating the delay when useing a remote server. values of 100 to 400 probably reasonable.
@@ -240,5 +267,8 @@ BUTTON_PRESS_NEW_TUB = False #when enabled, makes it easier to divide our data i
 
 HAVE_CAMERA = False
 CAMERA_TYPE = "None"
+IMAGE_W = 160
+IMAGE_H = 120
+IMAGE_DEPTH = 3 
 
 DRIVE_TYPE = "behavior_cloning"
